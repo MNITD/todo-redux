d57f0307b48223683ff511cb915473e71ae6e05d
MNITD <bogdanleblank@ukr.net>
2018-03-07 22:34:05 +0200

    added function with spread realization

---
+++ src/utils/spread.js
@@ -0,0 +1,56 @@
+/**
+ * Created by bogdan on 07.03.18.
+ */
+const expect = require('expect');
+const deepFreeze = require('deep-freeze');
+
+const addCounter  = (list) =>{
+  return [...list, 0];
+};
+
+const removeCounter = (list, index) =>{
+  return [...list.slice(0, index),
+      ...list.slice(index + 1 )]
+};
+
+const incrementCounter = (list, index) =>{
+    return [
+        ...list.slice(0, index),
+        ...list[index]++,
+        ...list.slice(index + 1 )]
+};
+
+const testAddCounter = () =>{
+    const listBefore = [];
+    const listAfter = [0];
+
+    deepFreeze(listBefore);
+
+    expect(
+        addCounter(listBefore)
+    ).toEqual(listAfter);
+};
+
+const testRemoveCounter = () =>{
+    const listBefore = [10, 15, 20];
+    const listAfter = [10, 20];
+
+    deepFreeze(listBefore);
+
+    expect(
+        removeCounter(listBefore, 1)
+    ).toEqual(listAfter);
+};
+
+const testIncrementCounter = () =>{
+    const listBefore = [10, 15, 20];
+    const listAfter = [10, 16, 20];
+
+    deepFreeze(listBefore);
+
+    expect(
+        incrementCounter(listBefore, 1)
+    ).toEqual(listAfter);
+};
+
+console.log('All test passed!');
