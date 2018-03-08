7e38867cff64761ca046b73b869fb4ebce5fba45
MNITD <bogdanleblank@ukr.net>
2018-03-08 12:51:40 +0200

    added combineReducers from scratch

--- src/redux/reducers/todo.reducer.js
+++ src/redux/reducers/todo.reducer.js
@@ -49,6 +49,18 @@ const visibilityFilter = (
     }
 };
 
+const conbineReducers = (reducers) =>{
+    return (state = {}, action) =>{
+      return Object.keys(reducers).reduce(
+          (nextState, key) => {
+              nextState[key] = reducers[key](state[key], action);
+              return nextState;
+          },
+          {}
+      );
+
+    };
+};
 
 const todoApp = combineReducers({todos, visibilityFilter});
 // const todoApp = (state ={}, action) => {
